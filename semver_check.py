9f8ed281389cb4004d54d08483074d3a3ccefa7f
bschwyn <ben.schwyn@gmail.com>
2018-07-19 08:25:08 -0700

    tests now run via "python tests"

--- semver_check.py
+++ semver_check.py
@@ -174,36 +174,6 @@ def get_cli_input(args):
 
 
 
-
-
-#.............................................
-#tests
-
-def test_cli(self):
-    #self.assertEqual(["hello", "world", "foo"], get_cli_input("hello     world   foo "))
-    pass
-    #should be false get_cli_input("   helo world  foo")
-
-def test_is_valid_input():
-    t1 = "1.3.6 1.4.2"
-    t2 = "1.7.9 1.3.5 0.0.2"
-    t3 = "4.2.3-beta 4.2.3-alpha"
-    t4 = "1.6 1.6.3"
-    print("tests pass if true")
-    print(True == is_input_valid(t1))
-    print(False == is_input_valid(t2))
-    print(True == is_input_valid(t3))
-    print(False == is_input_valid(t4))
-
-#run tests
-
-def tests():
-    test_is_valid_input()
-    test_cli
-
-
-
-
 def main():
 
     args = get_cli_input(sys.argv[1])
@@ -219,7 +189,6 @@ def main():
 
 if __name__=="__main__":
     main()
-    #tests()
 
 
 
--- tests.py
+++ tests.py
@@ -1,15 +1,27 @@
 import unittest
 
-from semantic_verification import *
+from semver_check import *
 
 class Test(unittest.TestCase):
+
+    def testtest(self):
+        self.assertEqual(5,5)
 
     def test_cli(self):
         self.assertEqual(["hello", "world", "foo"], get_cli_input("hello     world   foo "))
 
         #should be false get_cli_input("   helo world  foo")
 
-    def test_is_parse_to_semver(self):
-        parse_string_to_semver("123.456.789")
-        parse_string_to_semver("00.123.456")
-        parse_string_to_semver("0.01")
+    def test_is_valid_input(self):
+        t1 = "1.3.6 1.4.2"
+        t2 = "1.7.9 1.3.5 0.0.2"
+        t3 = "4.2.3-beta 4.2.3-alpha"
+        t4 = "1.6 1.6.3"
+        print("tests pass if true")
+        print(True == is_input_valid(t1))
+        print(False == is_input_valid(t2))
+        print(True == is_input_valid(t3))
+        print(False == is_input_valid(t4))
+
+if __name__ == '__main__':
+    unittest.main()
